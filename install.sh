a705a729e374a128bf4bf415cdc0ee2ecc874d94
fedemp <bpm230@gmail.com>
2016-12-24 01:18:14 +0000

    update install

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/env bash
 git submodule init
 git submodule update
-ln -s zshrc ~/.zshrc
-ln -s zprofile ~/.zprofile
-ln -s zshenv ~/.zshenv
-ln -s tmux.conf ~/.tmux.conf
-ln -s dir_colors ~/.dir_colors
-ln -s gtkrc-2.0 ~/.gtkrc-2.0
-ln -s functions ~/.functions
-ln -s config ~/.config
+ln -rs ./zshrc ~/.zshrc
+ln -rs zprofile ~/.zprofile
+ln -rs zshenv ~/.zshenv
+ln -rs tmux.conf ~/.tmux.conf
+ln -rs dir_colors ~/.dir_colors
+ln -rs gtkrc-2.0 ~/.gtkrc-2.0
+ln -rs functions ~/.functions
+ln -rs config ~/.config
